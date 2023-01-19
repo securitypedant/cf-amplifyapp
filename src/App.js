f0b6f25d36649663e1878a4ac9a213ad4ef5198d
Simon Thorpe <simon@thorpevillage.com>
2023-01-19 12:44:30 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Sample website for Simon's Cloudflare homework.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
